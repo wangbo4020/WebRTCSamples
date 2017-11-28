a29efb9d3f866001b869d7dec35c5766b1ac93f1
wangxing <wangxing@remotecare.cn>
2017-11-28 17:59:40 +0800

    记录上次输入的昵称与房间号。

--- RemoteCare/src/main/java/com/adups/remotecare/MainActivity.java
+++ RemoteCare/src/main/java/com/adups/remotecare/MainActivity.java
@@ -1,6 +1,8 @@
 package com.adups.remotecare;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.text.TextUtils;
@@ -10,29 +12,45 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+	private SharedPreferences mPref;
+	EditText mEtNickname;
+	EditText mEtRoomId;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		mPref = getSharedPreferences(getPackageName() + "_pref", Context.MODE_PRIVATE);
+
+		mEtNickname = findViewById(R.id.et_nickname);
+		mEtRoomId = findViewById(R.id.et_room_id);
+
+		if (mPref.contains(RoomActivity.EXTRA_NICKNAME)) {
+			mEtNickname.setText(mPref.getString(RoomActivity.EXTRA_NICKNAME, ""));
+		}
+		if (mPref.contains(RoomActivity.EXTRA_ROOM_ID)) {
+			mEtRoomId.setText(mPref.getString(RoomActivity.EXTRA_ROOM_ID, ""));
+		}
 	}
 
 	public void onClickGoRoom(View view) {
-		EditText etNickname = findViewById(R.id.et_nickname);
-		EditText etRoomId = findViewById(R.id.et_room_id);
 
-		CharSequence nickname = etNickname.getText().toString();
-		CharSequence roomId = etRoomId.getText().toString();
+		CharSequence nickname = mEtNickname.getText().toString();
+		CharSequence roomId = mEtRoomId.getText().toString();
 
 		if (TextUtils.isEmpty(nickname)) {
 			Toast.makeText(this, "请输入昵称", Toast.LENGTH_SHORT).show();
-			etNickname.requestFocus();
+			mEtNickname.requestFocus();
 			return;
 		}
 		if (TextUtils.isEmpty(roomId)) {
 			Toast.makeText(this, "请输入房间号", Toast.LENGTH_SHORT).show();
-			etRoomId.requestFocus();
+			mEtRoomId.requestFocus();
 			return;
 		}
+
+		mPref.edit().putString(RoomActivity.EXTRA_NICKNAME, (String) nickname)
+				.putString(RoomActivity.EXTRA_ROOM_ID, (String) roomId).commit();
 
 		Intent intent = new Intent(this, RoomActivity.class);
 		intent.putExtra(RoomActivity.EXTRA_NICKNAME, nickname);
